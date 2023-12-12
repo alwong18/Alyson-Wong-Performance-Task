f46a51cfd2df66d9f0e7d869561d8c501f345a43
alwong18 <alwong18@s.sfusd.edu>
2023-12-12 11:53:39 -0800

    Player scene completed

---
+++ player.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var speed = 400
+var velocity
+
+func _process(delta):
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		
+	position += velocity * delta
